ee13b177163e0a9621bb123e8abd0c68e549f9b3
David Huffman <dshuffman25@hotmail.com>
2021-02-24 07:33:41 -0800

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	return render_template('index.html', name=name)
+	newName = name + " likes to juggle balls"
+	return render_template('index.html', name=newName)
 
 
 @app.route('/add_numbers', methods=['GET','POST'])
